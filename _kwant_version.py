dd9bb9265e90c24ef0644627f430ac31a7303e7b
klarc-T <142101103+klarc-T@users.noreply.github.com>
2025-05-23 09:29:34 -0300

    Add files via upload

---
+++ _kwant_version.py
@@ -0,0 +1,8 @@
+# This file will be overwritten by setup.py when a source or binary
+# distribution is made.  The magic value "__use_git__" is interpreted by
+# _common.py in this directory.
+version = "__use_git__"
+
+# These values are only set if the distribution was created with 'git archive'
+refnames = "HEAD -> main"
+git_hash = "442b296d"
